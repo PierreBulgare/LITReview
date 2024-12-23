c2c2f889380362f76c667155c81dc387b73b6ea1
Pierre BULGARE <pierre.bulgare@gmail.com>
2024-12-23 19:03:12 +0100

    Authentication Update

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'placeholder': "Nom d'utilisateur"})
+    )
+    password = forms.CharField(
+        max_length=30,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.label = ''
